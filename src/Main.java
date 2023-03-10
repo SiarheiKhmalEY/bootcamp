160193359ed8548e4f19dbbf0f3f72cc031999e4
Siarhei Khmal <siarhei.khmal@gds.ey.com>
2023-03-10 13:30:52 +0100

    pull request changes

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,19 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;1
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     private static final String INPUT = "sample-text-file.txt";
-    private static final String OUTPUT = "output.csv";
 
     public static void main(String[] args) {
         List<String> words = readFromFile(Path.of(INPUT));
-        Collections.sort(words);
-        writeToFile(words);
+        showResult(words);
     }
 
     public static List<String> readFromFile(Path path) {
@@ -26,24 +23,15 @@ public class Main {
                 stringList.add(bufferedReader.readLine());
             }
         } catch (IOException e) {
-            throw new RuntimeException("The file was not read");
+            throw new IllegalStateException("Can not read file from path " + path);
         }
         return stringList;
     }
 
-    public static void writeToFile(List<String> wordList) {
-        try (BufferedWriter bufferedWriter = Files.newBufferedWriter(Path.of(OUTPUT))) {
-            AtomicInteger id = new AtomicInteger();
-            bufferedWriter.write("ID Word" + "\n");
-            wordList.forEach(word -> {
-                try {
-                    bufferedWriter.write(id.getAndIncrement() + "  " + word + "\n");
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            });
-        } catch (IOException e) {
-            throw new RuntimeException("The file was not written");
-        }
+    public static void showResult(List<String> wordList) {
+        Collections.sort(wordList);
+        AtomicInteger id = new AtomicInteger(1);
+        System.out.println("ID,Word");
+        wordList.forEach(word -> System.out.println(id.getAndIncrement() + "," + word));
     }
 }
