5801cf1e0b76eff1aa305f4013dd4e0d6eddee3e
Siarhei Khmal <siarhei.khmal@gds.ey.com>
2023-04-11 14:24:44 +0200

    PR requested changes. Deleted comment code in POM.xml, add printing stack trace in logger.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,19 @@
+
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
-    private static final String INPUT = "sample-text-file2.txt";
+    private static final String INPUT = "sample-text-fil2e.txt";
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
@@ -34,7 +35,12 @@ public class Main {
                 stringList.add(bufferedReader.readLine());
             }
         } catch (IOException e) {
+            StringWriter sw = new StringWriter();
+            e.printStackTrace(new PrintWriter(sw));
+
             logger.error("Can not read file from path " + path);
+            logger.error(sw.toString());
+
             throw new IllegalStateException();
         }
         logger.warn("Reading data finished");
@@ -42,10 +48,13 @@ public class Main {
     }
 
     public  void showResult(List<String> wordList) {
+        logger.warn("Method showResult started");
+
         Collections.sort(wordList);
         AtomicInteger id = new AtomicInteger(1);
 
         logger.info("ID,Word");
         wordList.forEach(word -> logger.info(id.getAndIncrement() + "," + word));
+        logger.warn("Method showResult finished");
     }
 }
